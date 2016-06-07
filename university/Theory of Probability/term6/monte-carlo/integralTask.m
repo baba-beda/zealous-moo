feda11a823b850ccbfb57a6c8a19a1edc1ece818
baba-beda <darya.nightmare@gmail.com>
2016-06-08 00:12:43 +0300

    minor

--- university/Theory of Probability/term6/monte-carlo/integralTask.m
+++ university/Theory of Probability/term6/monte-carlo/integralTask.m
@@ -1,38 +1,34 @@
 function [] = integralTask()
   printf("Task 2:\nEstimate the definite integral of function f(x)\na) f(x) = ln(1 + x^2) with limits [2,5]\n");
-  [int1, disp1] = integralLog(10000);
-  [left1, right1] = confidenceInt(int1, 0.64, disp1, 10000);
+  [int1, left1, right1] = integralLog(10000);
   printf("n = %d: %f <= I <= %f\n", 10000, left1, right1);
-  [int2, disp2] = integralLog(1000000);
-  [left2, right2] = confidenceInt(int2, 0.64, disp2, 1000000);
+  [int2, left2, right2] = integralLog(1000000);
   printf("n = %d: %f <= I <= %f\n", 1000000, left2, right2);
   
   printf("\nb) f(x) = cos(x)exp(-(x+3)^2/4) with limits [-inf, inf]\n");
-  [int3, disp3] = integralExp(10000);
-  [left3, right3] = confidenceInt(int3, 0.64, disp3, 10000);
+  [int3, left3, right3] = integralExp(10000);
   printf("n = %d: %f <= I <= %f\n", 10000, left3, right3);
-  [int4, disp4]= integralExp(1000000);
-  [left4, right4] = confidenceInt(int4, 0.64, disp4, 1000000);
+  [int4, left4, right4]= integralExp(1000000);
   printf("n = %d: %f <= I <= %f\n", 1000000, left4, right4);
 endfunction
 
-function [integral, disp] = integralLog(n)
-  X = unifrnd(2, 5, n, 1);
-  y = logFunction(X);
+function [integral, left, right] = integralLog(n)
+  X = unifrnd(2, 5, 1, n);
+  y = logFunction(X) * 3;
   expectation = mean(y);
-  disp = dispersion(y) * 9;
-  integral = expectation * 3;
+  integral = expectation;
+  [left, right] = confidenceInt(y);
 endfunction
 
 function res = logFunction(X)
   res = log(1 + X .^ 2);
 endfunction
 
-function [res, disp] = integralExp(n)
+function [res, left, right] = integralExp(n)
   X = unifrnd(0, 1, 1, n);
   y = expFunction(X);
-  res = mean(y * 2);
-  disp = dispersion(y * 2);
+  res = mean(y);
+  [left, right] = confidenceInt(y);
 endfunction
   
 function res = expFunction(t)
@@ -40,6 +36,6 @@ function res = expFunction(t)
   two = t.^ 2;
   four = cos(-2 ./ t - 1);
   three = (1 - t).^2 ./ two;
-  res = (one + four) .* exp(-three) ./ two;
+  res = 2 * (one + four) .* exp(-three) ./ two;
 endfunction
 
