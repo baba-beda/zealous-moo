81885acadbd1b733409f3866e2a44bab1241913f
baba-beda <darya.nightmare@gmail.com>
2016-06-08 00:00:28 +0300

    minor

--- university/Theory of Probability/term6/monte-carlo/volumeTask.m
+++ university/Theory of Probability/term6/monte-carlo/volumeTask.m
@@ -1,10 +1,10 @@
-function [] = volumeTask()
-  printf("Task 1: \nEstimate the volume of {F(x) = f(x_1) + ... + f(x_k) <= c}, where x_i is from [0, 1], k - dimensionality, \nif c = %f, k = %d, f(t) = t^%d\n", 1.4, 6, 3); 
-  [vol1, disp1] = functionVolume(10000, 6, 1.4, 3);
+function [] = volumeTask(k, c, a)
+  printf("Task 1: \nEstimate the volume of {F(x) = f(x_1) + ... + f(x_k) <= c}, where x_i is from [0, 1], k - dimensionality, \nif c = %f, k = %d, f(t) = t^%f\n", c, k, a); 
+  [vol1, disp1] = functionVolume(10000, k, c, a);
   [left1, right1] = confidenceInt(vol1, 0.64, disp1, 10000);
   printf("n = %d: %f <= V <= %f\n", 10000, left1, right1);
   
-  [vol2, disp2] = functionVolume(1000000, 6, 1.4, 3);
+  [vol2, disp2] = functionVolume(1000000, k, c, a);
   [left2, right2] = confidenceInt(vol2, 0.64, disp2, 1000000);
   printf("n = %d: %f <= V <= %f\n", 1000000, left2, right2);
 endfunction
