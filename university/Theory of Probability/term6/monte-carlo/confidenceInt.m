d5988ee176a00e8ad0a362e26adfd764fe9d1714
baba-beda <darya.nightmare@gmail.com>
2016-06-08 00:05:47 +0300

    minor

--- university/Theory of Probability/term6/monte-carlo/confidenceInt.m
+++ university/Theory of Probability/term6/monte-carlo/confidenceInt.m
@@ -1,4 +1,8 @@
-function [left, right] = confidenceInt(mX, t, disp, n)
+function [left, right] = confidenceInt(X)
+  disp = dispersion(X);
+  n = size(X)(2);
+  t = 0.64;
+  mX = mean(X);
   left = mX - t * sqrt(disp) / n;
   right = mX + t * sqrt(disp) / n;
 endfunction
